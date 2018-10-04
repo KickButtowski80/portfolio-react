e8815d9a27d8a3ff426fee40861216a7a6bb16ac
Kick Buttowski <pazpaz22@yahoo.com>
2018-10-04 11:18:14 -0700

    added react-router-dom, create routes for root, create, edit, and help

--- src/app.js
+++ src/app.js
@@ -1,6 +1,52 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import 'normalize.css/normalize.css';
+import {BrowserRouter, Route} from 'react-router-dom';
 import './styles/styles.scss';
 
-ReactDOM.render(<p>This is my boilerplate for expnsify app</p>, document.getElementById('app'));
+const ExpenseDashboardPage = () => {
+    return(
+        <div>
+           This is from my dashboard component 
+        </div>
+    )
+}
+
+const AddExpensePage = () => {
+    return(
+        <div>
+           This is from my add exchangePage component 
+        </div>
+    )
+}
+
+const EditExpensePage = () => {
+    return(
+        <div>
+           This is from my edit exchangePage component 
+        </div>
+    )
+}
+
+const HelpPage = () => {
+    return(
+        <div>
+           This is from my Help component 
+        </div>
+    )
+}
+
+const routes = (
+    <BrowserRouter>
+        <div>
+            <Route path="/" component={ExpenseDashboardPage} exact={true}/>
+            <Route path="/create" component={AddExpensePage} />
+            <Route path="/edit" component={EditExpensePage} />
+            <Route path="/help" component={HelpPage} />
+        </div>
+        
+    
+    </BrowserRouter>
+)
+
+ReactDOM.render(routes, document.getElementById('app'));
