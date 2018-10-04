b851d20b5cd03ece1c7a70cb84a1bca0584f9243
Kick Buttowski <pazpaz22@yahoo.com>
2018-10-04 14:20:04 -0700

    got rid fo uncessary blank lines

--- src/routers/AppRouter.js
+++ src/routers/AppRouter.js
@@ -7,10 +7,6 @@ import AddExpensePage from '../components/AddExpensePage';
 import ExpenseDashboardPage from '../components/ExpenseDashboardPage';
 import Header from '../components/Header'; 
 
-
-
-
-
 const AppRouter  = () =>{
     return(
         <BrowserRouter>
